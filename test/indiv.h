3b662658d78bbabbba0778158c6cc5888a18afc3
WFOudijk <w.f.oudijk@student.rug.nl>
2022-12-09 10:46:41 +0100

    test

---
+++ test/indiv.h
@@ -0,0 +1,13 @@
+//
+//  indiv.h
+//  test
+//
+//  Created by Willemijn Oudijk on 06/12/2022.
+//
+
+#ifndef indiv_h
+#define indiv_h
+
+
+
+#endif /* indiv_h */
---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Willemijn Oudijk on 06/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
